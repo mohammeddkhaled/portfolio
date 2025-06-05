82cab396fa5f7b0f40ff7c25a643dd459f33d8ba
mohammedkhaled <mohdkhaled1031@gmail.com>
2025-06-05 21:52:22 +0530

    correct method to post

--- frontend/script.js
+++ frontend/script.js
@@ -64,7 +64,7 @@ document.getElementById('contact-form').addEventListener('submit', async functio
 
   try {
     const res = await fetch("https://portfolio-backend-2o6z.onrender.com/submit", {
-      method: 'GET',
+      method: 'post',
       headers: {
         'Content-Type': 'application/json',
       },
